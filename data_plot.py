c9189fdf35de9c23e512a5003f5fa4773db97793
Virtue Zhao <virtuedizhao@gmail.com>
2024-07-27 11:55:01 +1200

    update

--- data_plot.py
+++ data_plot.py
@@ -46,6 +46,7 @@ axes[0, 0].legend(handles=legend_elements, loc='lower right')
 # Simulate covariate shift by adding some noise
 s_class_1_covariate = s_class_1 + np.random.normal(0, 0.75, s_class_1.shape)
 s_class_2_covariate = s_class_2 + np.random.normal(0, 0.75, s_class_2.shape)
+# s_class_1_covariate = s_class_1_covariate + [1, 0]
 s_class_2_covariate = s_class_2_covariate + [1, 0]
 axes[0, 1].scatter(s_class_1[:, 0], s_class_1[:, 1], color='red', marker='.', label='Class 1 (Source)')
 axes[0, 1].scatter(s_class_2[:, 0], s_class_2[:, 1], color='blue', marker='.', label='Class 2 (Source)')
@@ -70,22 +71,46 @@ legend_elements = [
     Line2D([0], [0], marker='s', color='w', markerfacecolor='blue', markersize=10, label='Class 2'),
     Line2D([0], [0], marker='.', color='w', markerfacecolor='black', markersize=10, label='Source'),
     Line2D([0], [0], marker='*', color='w', markerfacecolor='black', markersize=10, label='Target'),
-    Line2D([0], [0], marker='^', color='w', markerfacecolor='black', markersize=10, label='CoAug'),
+    Line2D([0], [0], marker='^', color='w', markerfacecolor='black', markersize=10, label='CovAug'),
 ]
 axes[0, 1].legend(handles=legend_elements, loc='lower right')
 
 # Conditional Distribution Shift Plot
 # Simulate conditional distribution shift by shifting means
-# class_1_cond_shift = class_1 + [1.5, 1.5]
-# class_2_cond_shift = class_2 + [-1.5, -1.5]
+np.random.seed(826)
+s_class_1_conditional = s_class_1 + np.random.normal(0, 0.75, s_class_1.shape)
+s_class_2_conditional = s_class_2 + np.random.normal(0, 0.75, s_class_2.shape)
+s_class_1_conditional = s_class_1_conditional + [1.5, 1]
+s_class_2_conditional = s_class_2_conditional + [3, -1]
 axes[1, 0].scatter(s_class_1[:, 0], s_class_1[:, 1], color='red', marker='.', label='Class 1 (Source)')
 axes[1, 0].scatter(s_class_2[:, 0], s_class_2[:, 1], color='blue', marker='.', label='Class 2 (Source)')
 axes[1, 0].scatter(t_class_1[:, 0], t_class_1[:, 1], color='red', marker='*', label='Class 1 (Target)')
 axes[1, 0].scatter(t_class_2[:, 0], t_class_2[:, 1], color='blue', marker='*', label='Class 2 (Target)')
-# axes[2].scatter(class_1_cond_shift[:, 0], class_1_cond_shift[:, 1], color='pink', marker='x', label='Class 1 (Conditional Shift)')
-# axes[2].scatter(class_2_cond_shift[:, 0], class_2_cond_shift[:, 1], color='purple', marker='x', label='Class 2 (Conditional Shift)')
+axes[1, 0].scatter(s_class_1_conditional[:, 0], s_class_1_conditional[:, 1], color='red', marker='X', label='Class 1 (Conditional Shift)')
+axes[1, 0].scatter(s_class_2_conditional[:, 0], s_class_2_conditional[:, 1], color='blue', marker='X', label='Class 2 (Conditional Shift)')
+
+x = np.vstack((s_class_1, s_class_1_conditional, s_class_2, s_class_2_conditional))
+y = np.hstack((np.ones(num_data * 2), np.zeros(num_data * 2)))
+clf = LogisticRegression()
+clf.fit(x, y)
+all_data = np.vstack((s_class_1, s_class_1_conditional, s_class_2, s_class_2_conditional, t_class_1, t_class_2))
+x_min, x_max = all_data[:, 0].min() - 1, all_data[:, 0].max() + 1
+y_min, y_max = all_data[:, 1].min() - 1, all_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+z = clf.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+axes[1, 0].contourf(xx, yy, z, alpha=0.3, cmap=plt.cm.Paired)
 axes[1, 0].set_title('Conditional Distribution Shift Augmentation')
+legend_elements = [
+    Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10, label='Class 1'),
+    Line2D([0], [0], marker='s', color='w', markerfacecolor='blue', markersize=10, label='Class 2'),
+    Line2D([0], [0], marker='.', color='w', markerfacecolor='black', markersize=10, label='Source'),
+    Line2D([0], [0], marker='*', color='w', markerfacecolor='black', markersize=10, label='Target'),
+    Line2D([0], [0], marker='X', color='w', markerfacecolor='black', markersize=10, label='ConAug'),
+]
 axes[1, 0].legend(handles=legend_elements, loc='lower right')
+
+
+
 
 # Covariate and Conditional Distribution Shift
 axes[1, 1].scatter(s_class_1[:, 0], s_class_1[:, 1], color='red', marker='.', label='Class 1 (Source)')
