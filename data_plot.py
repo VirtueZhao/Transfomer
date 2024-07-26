dd4cab8442c58192c55c18f268aadbd7a73f4beb
Virtue Zhao <virtuedizhao@gmail.com>
2024-07-26 22:01:22 +1200

    update

--- data_plot.py
+++ data_plot.py
@@ -13,6 +13,7 @@ fig, axes = plt.subplots(2, 2, figsize=(12, 12))
 np.random.seed(0)
 s_class_1 = np.random.multivariate_normal([2, 2], [[1, 0], [0, 1]], num_data)
 s_class_2 = np.random.multivariate_normal([-2, -2], [[1, 0], [0, 1]], num_data)
+s_class_2 = s_class_2 + [1, 0]
 np.random.seed(42)
 t_class_1 = np.random.multivariate_normal([2, 2], [[1, 0], [0, 1]], num_data) + [6, 1]
 t_class_2 = np.random.multivariate_normal([-2, -2], [[1, 0], [0, 1]], num_data) + [8, -2]
@@ -43,14 +44,15 @@ axes[0, 0].legend(handles=legend_elements, loc='lower right')
 
 # Covariate Shift Plot
 # Simulate covariate shift by adding some noise
-s_class_1_covariate = s_class_1 + np.random.normal(0, 1, s_class_1.shape)
-s_class_2_covariate = s_class_2 + np.random.normal(0, 1, s_class_2.shape)
+s_class_1_covariate = s_class_1 + np.random.normal(0, 0.75, s_class_1.shape)
+s_class_2_covariate = s_class_2 + np.random.normal(0, 0.75, s_class_2.shape)
+s_class_2_covariate = s_class_2_covariate + [1, 0]
 axes[0, 1].scatter(s_class_1[:, 0], s_class_1[:, 1], color='red', marker='.', label='Class 1 (Source)')
 axes[0, 1].scatter(s_class_2[:, 0], s_class_2[:, 1], color='blue', marker='.', label='Class 2 (Source)')
 axes[0, 1].scatter(t_class_1[:, 0], t_class_1[:, 1], color='red', marker='*', label='Class 1 (Target)')
 axes[0, 1].scatter(t_class_2[:, 0], t_class_2[:, 1], color='blue', marker='*', label='Class 2 (Target)')
-axes[0, 1].scatter(s_class_1_covariate[:, 0], s_class_1_covariate[:, 1], marker='x', color='red', label='Class 1 (Covariate Shift)')
-axes[0, 1].scatter(s_class_2_covariate[:, 0], s_class_2_covariate[:, 1], marker='x', color='blue', label='Class 2 (Covariate Shift)')
+axes[0, 1].scatter(s_class_1_covariate[:, 0], s_class_1_covariate[:, 1], marker='^', color='red', label='Class 1 (Covariate Shift)')
+axes[0, 1].scatter(s_class_2_covariate[:, 0], s_class_2_covariate[:, 1], marker='^', color='blue', label='Class 2 (Covariate Shift)')
 
 x = np.vstack((s_class_1, s_class_1_covariate, s_class_2, s_class_2_covariate))
 y = np.hstack((np.ones(num_data * 2), np.zeros(num_data * 2)))
@@ -63,6 +65,13 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
 z = clf.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
 axes[0, 1].contourf(xx, yy, z, alpha=0.3, cmap=plt.cm.Paired)
 axes[0, 1].set_title('Covariate Shift Augmentation')
+legend_elements = [
+    Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=10, label='Class 1'),
+    Line2D([0], [0], marker='s', color='w', markerfacecolor='blue', markersize=10, label='Class 2'),
+    Line2D([0], [0], marker='.', color='w', markerfacecolor='black', markersize=10, label='Source'),
+    Line2D([0], [0], marker='*', color='w', markerfacecolor='black', markersize=10, label='Target'),
+    Line2D([0], [0], marker='^', color='w', markerfacecolor='black', markersize=10, label='CoAug'),
+]
 axes[0, 1].legend(handles=legend_elements, loc='lower right')
 
 # Conditional Distribution Shift Plot
